bb12fd0acdfe9436a6984b70305e9ad57dda86b9
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-01 23:25:43 +0100

    CSS Remaining

--- src/Components/bottles/bottle.jsx
+++ src/Components/bottles/bottle.jsx
@@ -1,10 +1,9 @@
-import 'Components/bottle.css'
-export default function Bottle({bottle}) {
+export default function Bottle({ bottle }) {
     console.log(bottle);
     return (
         <div className="bottle-container">
-           <h2>{bottle.brand}</h2>
-           <img src={bottle.image} alt="" />
+            <h2>{bottle.brand}</h2>
+            <img src={bottle.image} alt="" />
         </div>
     )
 }
--- src/Components/bottles/bottles.jsx
+++ src/Components/bottles/bottles.jsx
@@ -1,5 +1,6 @@
-import { use } from "react"
+import { use } from "react";
 import Bottle from "./bottle";
+import './App.css';
 
 export default function Bottles({bottlesPromise}) {
     const bottles = use(bottlesPromise);
