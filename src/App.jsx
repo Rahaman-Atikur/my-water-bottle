eb0ea0a60cc127224c90ecad3027e7f12483b4d8
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-01 21:20:28 +0100

    Component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Suspense } from 'react'
 import './App.css'
-
+import Bottles from './Components/bottles/bottles'
+// const bottle = [
+//   { id: 1, name: 'Atikur', price: 250, color: 'pink' },
+//   { id: 1, name: 'Shuvo', price: 300, color: 'red' },
+//   { id: 1, name: 'Trisha', price: 150, color: 'yellow' },
+//   { id: 1, name: 'okaToLight', price: 450, color: 'pink' },
+//   { id: 1, name: 'NoPowerNoGame', price: 850, color: 'Green' }
+// ]
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My Water Bottle</h1>
+      <Suspense>
+        <Bottles></Bottles>
+      </Suspense>
     </>
   )
 }
-
 export default App
