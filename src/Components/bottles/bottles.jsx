62506ae3a68b59c2d778b296abac6d2d15e3f7b2
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-02 23:18:39 +0100

    Cart component is not done

--- src/Components/bottles/bottles.jsx
+++ src/Components/bottles/bottles.jsx
@@ -1,6 +1,7 @@
 import { use, useEffect, useState } from "react";
 import Bottle from "./bottle";
 import { addItSt, getCartLS } from "../../LocalStorage";
+
 export default function Bottles({ bottlesPromise }) {
     const [cart, setCart] = useState([]);
     const bottles = use(bottlesPromise);
@@ -15,19 +16,17 @@ export default function Bottles({ bottlesPromise }) {
         }
         setCart(storedCart);
     }, [bottles]);
-
-
-    // console.log(bottles);
+    console.log(bottles);
     const handleAddToCart = (bottle) => {
         const newCart = [...cart, bottle];
         setCart(newCart);
-
         addItSt(bottle.id);
     }
     return (
         <div>
             <h1>Bottles: {bottles.length}</h1>
             <h2>Added tio the cart:{cart.length}</h2>
+            <Cart cart={cart}></Cart>
             <div className="grid grid-cols-3">
                 {
                     bottles.map(bottle => <Bottle handleAddToCart={handleAddToCart} key={bottle.id} bottle={bottle}></Bottle>)
